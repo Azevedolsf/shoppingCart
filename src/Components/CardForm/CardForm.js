bb2b08ad58889abd35ebb69a3ea2d00f92e23f26
azevedolsf <azevedogmp@gmail.com>
2021-11-19 00:21:49 -0300

    subindo component card completo

--- src/Components/CardForm/CardForm.js
+++ src/Components/CardForm/CardForm.js
@@ -13,7 +13,19 @@ const CardForm = () => {
                 <label htmlFor="">Card Number</label>
                 <input type="password" placeholder="****   ****   ****   ****"/>
             </fieldset>
-
+            <div className={styles.fieldSetSeparator}>
+                <fieldset>
+                    <label htmlFor="">Expiration Date</label>
+                    <div>
+                        <input type="text" placeholder="mm" />
+                        <input type="text" placeholder="yyyy" />
+                    </div>
+                </fieldset>
+                <fieldset>
+                    <label htmlFor="">CVV</label>
+                    <input type="password" placeholder="XXX"/>
+                </fieldset>           
+            </div>
             <button className={styles.cardBtn}>
                 Check Out
             </button>
